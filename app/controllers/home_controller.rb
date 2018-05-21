44df97eb2fe942598379c839eb292691a6dbeefb
Puneet <puneetgupta18190@gmail.com>
2018-05-21 22:38:17 +0530

    api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -34,11 +34,42 @@ class HomeController < ApplicationController
       redirect_to "/questions/#{params[:id].to_i+1}"
     elsif params[:id].to_i == 8
       Answer.create(user_id: current_user.id, data: session["info"])
+      call_crm
       session["info"]=[]
       redirect_to "/thank-you"
     else
       redirect_to root_url, alert: 'Invalid Question'
     end  
+  end
+
+  def call_crm
+    begin
+      q1=session["info"].select{|b| b["id"]=="1"}.first
+      q2=session["info"].select{|b| b["id"]=="2"}.first
+      q3=session["info"].select{|b| b["id"]=="3"}.first
+      q4=session["info"].select{|b| b["id"]=="4"}.first
+      q5=session["info"].select{|b| b["id"]=="5"}.first
+      q6=session["info"].select{|b| b["id"]=="6"}.first
+      q7=session["info"].select{|b| b["id"]=="7"}.first
+      q8=session["info"].select{|b| b["id"]=="8"}.first
+
+      res = HTTParty.post("https://www.tapapp.com/2734856/LeadImport/Scorecard.aspx", :query => { 
+        name: current_user.name,  
+        email: current_user.email,
+        question_1: { score: q1['score'], goal: q1['goal'], feedback: q1['feedback'] },
+        question_2: { score: q2['score'], goal: q2['goal'], feedback: q2['feedback'] },
+        question_3: { score: q3['score'], goal: q3['goal'], feedback: q3['feedback'] },
+        question_4: { score: q4['score'], goal: q4['goal'], feedback: q4['feedback'] },
+        question_5: { score: q5['score'], goal: q5['goal'], feedback: q5['feedback'] },
+        question_6: { score: q6['score'], goal: q6['goal'], feedback: q6['feedback'] },
+        question_7: { score: q7['score'], goal: q7['goal'], feedback: q7['feedback'] },
+        question_8: { score: q8['score'], goal: q8['goal'], feedback: q8['feedback'] }
+      })
+      puts "API runs sucessfully."
+      puts res
+    rescue Exception => e
+      puts "Exception in API: #{e.message}"
+    end
   end
 
   def completed
