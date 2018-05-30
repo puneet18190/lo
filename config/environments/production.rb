fe5aca367aca7fb5afe2ca313168daab73446607
Puneet <puneetgupta18190@gmail.com>
2018-05-30 21:53:40 +0530

    code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'http://loan-potential.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "puneet.heroku@gmail.com",
+    :password => "puneet.18",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
 end
